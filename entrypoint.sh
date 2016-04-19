d423119c2e9e3a82b151fc957f62da861cbca2d9
Sébastien GLON <sebastien.glon@netapsys.fr>
2016-04-19 07:19:00 +0000

    xupdate config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ echo "param @ $@"
 
 if [ "$1" = 'register' ]; then
     shift
-    DOCKING_CONFIG=/register register  
-        -ip="$HOST_IP" \
-        -r="$REGISTER_URL" \
-        -d="$DOCKER_URL" \
+    DOCKING_CONFIG=/register register \
+        -ip=${HOST_IP} \
+        -r=${REGISTER_URL} \
+        -d=${DOCKER_URL} \
         "$@"
 else
     exec "$@"
